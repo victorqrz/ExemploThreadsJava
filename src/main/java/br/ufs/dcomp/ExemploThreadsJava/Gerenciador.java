d1137669f7bb779df2ecfc545ec9e5831873bc70
Victor Queiroz <victorqrz@academico.ufs.br>
2025-02-07 23:01:26 +0000

    exemplo novo remedio

--- src/main/java/br/ufs/dcomp/ExemploThreadsJava/Gerenciador.java
+++ src/main/java/br/ufs/dcomp/ExemploThreadsJava/Gerenciador.java
@@ -10,10 +10,12 @@ public class Gerenciador
     {
         Lembrete l1 = new Lembrete("Dorflex", 10, 4);
         Lembrete l2 = new Lembrete("Pracetamol", 8, 3);
-        Lembrete l3 = new Lembrete("Piroxican", 4, 8);
+        Lembrete l3 = new Lembrete("Cataflam", 4, 8);
+        Lembrete l4 = new Lembrete("Doralgina", 2, 4);
         
         l1.start();
         l2.start();
         l3.start();
+        l4.start();
     }
 }
